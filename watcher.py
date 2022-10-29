7cb5cc0b41065042856fe1c81d209ddfdb86ce12
Yisheng Gong <yisheng_gong@onmail.com>
2022-10-29 15:14:56 -0400

    add comments

--- watcher.py
+++ watcher.py
@@ -10,6 +10,9 @@ DIR_PICK_TYPE = 2
 
 class FileWatcher(object):
     def __init__(self, path, interval=60, qmaxsize=1000):
+        """
+        Initialize file watcher
+        """
         self.paths = self.parse_path(path) # file paths to watch
         self.dirs = [] # dir paths to watch for new files
         self.handlers = [] # a list of handlers
@@ -22,6 +25,11 @@ class FileWatcher(object):
         self.stop_watch_flag = False
     
     def parse_path(self, path, pick_type=FILE_PICK_TYPE):
+        """
+        parse unix-style path to a list of paths,
+        e.g. /var/log/*.log -> ["/var/log/a.log", "/var/log/b.log"]
+        you can choose either file or dir as the result
+        """
         _path = []
         try:
             for p in glob(path):
@@ -37,6 +45,9 @@ class FileWatcher(object):
         return _path
     
     def add_path(self, path):
+        """
+        add file path to watch
+        """
         _path = self.parse_path(path)
         for p in _path:
             if p not in self.fds.keys():
@@ -44,14 +55,20 @@ class FileWatcher(object):
                 self.progress[p] = 0
     
     def add_dir(self, path):
+        """
+        add dir path to watch
+        """
         _path = self.parse_path(path, pick_type=DIR_PICK_TYPE)
         for p in _path:
             self.dirs.append(p)
 
     def open(self, path):
+        """
+        open file descriptor and seek to the beginning of the file
+        """
         try:
             f = open(path, "r")
-            f.seek(0, os.SEEK_END)
+            f.seek(0)
             if not f.readable():
                 f.close()
                 logging.warning(f"File not readable: path={path}")
@@ -63,11 +80,17 @@ class FileWatcher(object):
         return f
     
     def close(self, path):
+        """
+        close file descriptor
+        """
         if path in self.fds.keys():
             self.fds[path].close()
             self.fds[path] = None
 
     async def watch_file(self, path):
+        """
+        watch file and put new lines into queue
+        """
         while not self.stop_watch_flag:
             try:
                 f = self.fds[path]
@@ -82,6 +105,9 @@ class FileWatcher(object):
             await asyncio.sleep(self.interval)
 
     async def watch_dir(self, path):
+        """
+        watch dir and create new coroutines to watch new files
+        """
         while not self.stop_watch_flag:
             _path = self.parse_path(path)
             for p in _path:
@@ -91,18 +117,30 @@ class FileWatcher(object):
             await asyncio.sleep(self.interval)
     
     def stop(self):
+        """
+        stop watching
+        """
         self.stop_watch_flag = True
         for p in self.fds.keys():
             self.close(p)
 
     def commit(self, path, offset):
+        """
+        commit progress
+        """
         self.progress[path] += offset
 
     def put(self, path, msg):
+        """
+        put new message into queue
+        """
         if msg:
             self.queue.put({"path": path, "msg": msg})
 
     def get(self):
+        """
+        get message from queue
+        """
         _json = self.queue.get()
         path = _json["path"]
         msg = _json["msg"]
@@ -110,6 +148,9 @@ class FileWatcher(object):
         return msg.strip()
 
     def seek(self, path, offset, whence=0):
+        """
+        set file pointer to a specific position
+        """
         _path = self.parse_path(path)
         for p in _path:
             if p in self.fds.keys():
@@ -120,15 +161,24 @@ class FileWatcher(object):
                                     whence={whence}, msg={str(e)}")
     
     def register_handler(self, handler):
+        """
+        register a handler to handle messages
+        """
         self.handlers.append(handler)
 
     async def handle_msg(self, msg):
+        """
+        handle message
+        """
         for handler in self.handlers:
             if handler.match(msg):
                 for func in handler.funcs:
                     await func(msg)
     
     async def run(self):
+        """
+        run file watcher
+        """
         tasks = [asyncio.create_task(self.watch_file(p)) for p in self.paths]
         tasks += [asyncio.create_task(self.watch_dir(p)) for p in self.dirs]
         tasks += [asyncio.create_task(self.handle_msg(self.get()))]
